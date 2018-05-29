9f6089f5eb014e9b382eb0baf6a70fd9b0273e71
Icchan <nepif168@gmail.com>
2018-05-29 22:00:48 +0900

    ajaxエラー残り

---
+++ Templates/template.js
@@ -0,0 +1,12 @@
+function header(){
+
+    $.ajax({
+        url: "header.html",
+        cache: false,
+        async: false,
+        dataType: 'html',
+        success: function(html){
+            document.write(html);
+        },
+    });
+}
